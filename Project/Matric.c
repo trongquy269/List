15b484e24e78043f00d59b22f5a9fb203832d0d6
NT Quy <quyb2017000@student.ctu.edu.vn>
2022-03-08 18:27:03 +0700

    Plus and subtract 2 matrix

--- Project/Matric.c
+++ Project/Matric.c
@@ -7,8 +7,111 @@ typedef struct
 {
     int row;
     int column;
-    int M[MAX][MAX];
+    float M[MAX][MAX];
 } MATRIX;
+
+void Plus_2_matrix(MATRIX *m, MATRIX *n)
+{
+    if ((m->row == n->row) && (m->column == n->column))
+    {
+        for (int i = 0; i < m->row; i++)
+        {
+            for (int j = 0; j < m->column; j++)
+            {
+                m->M[i][j] += n->M[i][j];
+            }
+        }
+    }
+    else
+    {
+        printf("\n2 matrix are not of the same level!");
+        if ((m->row != n->row) && (m->column != n->column))
+        {
+            printf("\nROW: %d is not equal to %d", m->row, n->row);
+            printf("\nCOLUMN: %d is not equal to %d\n", m->column, n->column);
+        }
+        else if (m->row != n->row)
+        {
+            printf("\nROW: %d is not equal to %d\n", m->row, n->row);
+        }
+        else
+        {
+            printf("\nCOLUMN: %d is not equal to %d\n", m->column, n->column);
+        }
+    }
+}
+
+void Subtract_2_matrix(MATRIX *m, MATRIX *n)
+{
+    if ((m->row == n->row) && (m->column == n->column))
+    {
+        for (int i = 0; i < m->row; i++)
+        {
+            for (int j = 0; j < m->column; j++)
+            {
+                m->M[i][j] -= n->M[i][j];
+            }
+        }
+    }
+    else
+    {
+        printf("\n2 matrix are not of the same level!");
+        if ((m->row != n->row) && (m->column != n->column))
+        {
+            printf("\nROW: %d is not equal to %d", m->row, n->row);
+            printf("\nCOLUMN: %d is not equal to %d\n", m->column, n->column);
+        }
+        else if (m->row != n->row)
+        {
+            printf("\nROW: %d is not equal to %d\n", m->row, n->row);
+        }
+        else
+        {
+            printf("\nCOLUMN: %d is not equal to %d\n", m->column, n->column);
+        }
+    }
+}
+
+void Multiply_aNumber(MATRIX *m, float x)
+{
+    for (int i = 0; i < m->row; i++)
+    {
+        for (int j = 0; j < m->column; j++)
+        {
+            m->M[i][j] *= x;
+        }
+    }
+}
+
+// void Multiply_2_matrix(MATRIX *m, MATRIX *n)
+// {
+//     if (m->row  == m->column)
+//     {
+//         MATRIX *temp = (MATRIX *)malloc(sizeof(MATRIX));
+//         temp->row = m->row;
+//         temp->column = n->column;
+//         for (int i = 0; i < m->row; i++)
+//         {
+//             for (int k = 0; k < n->column; k++)
+//             {
+//                 for (int j = 0; j < m->column; j++)
+//                 {
+//                     temp->M[i][k] += m->M[i][j] * n->M[j][k];
+//                 }
+//             }
+//         }
+//         MATRIX *m = (MATRIX *)malloc(sizeof(MATRIX));
+//         m->row = temp->row;
+//         m->column = temp->column;
+//         for (int i = 0; i < m->row; i++)
+//         {
+//             for (int j = 0; j < m->column; j++)
+//             {
+//                 m->M[i][j] = temp->M[i][j];
+//             }
+//         }
+//     }
+// }
 
 void Input(MATRIX *m)
 {
@@ -42,6 +145,9 @@ int main()
 {
     MATRIX *m = (MATRIX *)malloc(sizeof(MATRIX));
     Input(m);
+    MATRIX *n = (MATRIX *)malloc(sizeof(MATRIX));
+    Input(n);
+    //Multiply_2_matrix(m, n);
     Output(*m);
     return 0;
 }
